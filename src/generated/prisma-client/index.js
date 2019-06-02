8e81e1b3b1b84f013a3153b6cec84918876c93e0
Mariusz <cieslak.marius@gmail.com>
2019-06-02 10:52:22 +0200

    Initial commit

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Odpowiedz",
+    embedded: false
+  },
+  {
+    name: "Pytanie",
+    embedded: false
+  },
+  {
+    name: "Uzytkownik",
+    embedded: false
+  },
+  {
+    name: "Ankieta",
+    embedded: false
+  },
+  {
+    name: "Odpowiedzi",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/mariusz-cieslak/GraphQL/dev`
+});
+exports.prisma = new exports.Prisma();
---
+++ src/index.js
@@ -0,0 +1,89 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
+const {forwardTo} = require('prisma-binding')
+var jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const resolvers = {
+  Query: {
+    ankieta: forwardTo('db'),
+    pobierzListeAnkiet: (root, args, context, info) => {
+      return context.prisma.ankietas(null, info)
+    },
+    wynikiAnkiety: (root, args, context, info) => {
+      return null
+    },
+    logowanie: async (root, args, context, info) => {
+
+      var result = await context.prisma.uzytkownik({login: args.login})
+
+      if(!bcrypt.compareSync(args.haslo, result.haslo)) { throw new Exception("Haslo niepoprawne")}
+
+      return context.prisma.uzytkownik({login: args.login}, info)
+    }
+  },
+  Mutation: {
+    rejestracja: (root, args, context, info) => {
+      var hash = bcrypt.hashSync(args.haslo, saltRounds);
+
+      var token = jwt.sign(
+        { 
+          login: args.login,
+          haslo: args.haslo
+        }, 'dupskoxD')
+
+      return context.prisma.createUzytkownik({
+        login: args.login, 
+        haslo: hash, 
+        token: token}, info)
+    },
+    dodajAnkiete: (root, args, context, info) => {
+      var mutationString = `{`
+      mutationString += `"nazwaAnkiety": "${args.data.nazwaAnkiety}",`
+      mutationString += `"pytania": {`
+      mutationString += `"create": [`
+      var pytanieStr = ""
+      args.data.pytania.forEach((pytanie, key, arr) => { 
+        pytanieStr = `{`
+        pytanieStr += `"numerPytania": ` + pytanie.numerPytania + `,`
+        pytanieStr += `"trescPytnia": "` + pytanie.trescPytnia + `",`
+        pytanieStr += `"pytanieKontrolne": ` + pytanie.pytanieKontrolne + `,`
+        pytanieStr += `"reprezentacjaPytania": "` + pytanie.reprezentacjaPytania + `",`
+        pytanieStr += `"odpowiedzi": { "create" :[`
+        pytanie.odpowiedzi.forEach((odpowiedzTem, keyOdp, arrOdp) => {
+          if (Object.is(arrOdp.length - 1, keyOdp)) {
+            pytanieStr += `{ "nrOdpowiedzi": ${odpowiedzTem.nrOdpowiedzi} ,"odpowiedz": "${odpowiedzTem.odpowiedz}"}`
+          } else {
+            pytanieStr += `{ "nrOdpowiedzi": ` + odpowiedzTem.nrOdpowiedzi + `,`
+            pytanieStr += `"odpowiedz": "` + odpowiedzTem.odpowiedz + `"},`
+          }
+        })
+        if (Object.is(arr.length - 1, key)){
+          pytanieStr += `]}`
+        }
+        else {
+          pytanieStr += `]},`  
+      }})
+      mutationString += pytanieStr
+
+      mutationString += "}]}}"
+
+      console.log(mutationString)
+
+      return context.prisma.createAnkieta(JSON.parse(mutationString))
+    },
+    przeslijOdpowiedzi: (root, args, context, info) => {
+      return null
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: { 
+    prisma
+},
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
